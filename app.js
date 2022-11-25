0594a296b47e18d037e693900fab9d3a79a36d7a
FeeinCof <tuan.letrong.dev@gmail.com>
2022-11-25 15:13:39 +0700

    Fix Port Heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
+var port = process.env.PORT || 3000;
 app.use(express.static('views'));
 app.get('/', (req, res) => {
     res.send('index.html');
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     return console.log('App is listening on port 3000');
 })
